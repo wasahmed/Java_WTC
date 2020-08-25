008e22d6d250940a897a8b5c41cc9fb53354e0d0
Waseem Ahmed <wasahmed@student.wethinkcode.co.za>
2020-08-25 21:22:15 +0200

    created an Address class that can be assigned to a person through getters and setters... Address becomes an attribute of the Person Class

--- src/com/vogella/javastarter/exercises1/Main.java
+++ src/com/vogella/javastarter/exercises1/Main.java
@@ -2,7 +2,26 @@ package com.vogella.javastarter.exercises1;
 
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person("john", "snow", 20);
-        System.out.println(person);
+        Person person1 = new Person("john", "snow", 20);
+        Person person2 = new Person("jill", "william", 20);
+        System.out.println(person1);
+        System.out.println(person2);
+
+        // Jill gets married to john
+        person2.setLastname(person1.getLastname());
+        System.out.println(person2);
+
+        Address address = new Address();
+        address.setCity("PTA");
+        address.setCountry("SA");
+        address.setNumber("1");
+        address.setPostalCode("12345");
+        address.setStreet("WTC street");
+
+        person1.setAddress(address);
+        address = null;
+
+        person1.getAddress().setNumber("2");
+        System.out.println(person1.getAddress());
     }
 }
--- src/com/vogella/javastarter/exercises1/Person.java
+++ src/com/vogella/javastarter/exercises1/Person.java
@@ -4,9 +4,10 @@ package com.vogella.javastarter.exercises1;
 public class Person {
 //    Add three instance variables to it, one for storing the first name of the person, one for storing the last name and one for storing the
 //    age of the Person.
-    private String name;
-    private  String lastname;
-    private int age;
+    String name;
+    String lastname;
+    int age;
+    private Address address;
 
 //    Use the constructor of the Person object to set the values to some default value.
     public Person(String name, String lastname, int age) {
@@ -15,7 +16,39 @@ public class Person {
         this.age = age;
     }
 
-//    Add a toString method.  This method is used to convert the object to a String representation.
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    //    Add a toString method.  This method is used to convert the object to a String representation.
     @Override
     public String toString() {
         return "Person{" +
