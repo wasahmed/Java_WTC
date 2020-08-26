da12aa1e6664f5d5fe74fd7df71dbf23efa42be5
Waseem Ahmed <wasahmed@student.wethinkcode.co.za>
2020-08-26 20:03:14 +0200

    Looping

---
+++ src/com/vogella/javastarter/loop/Loops.java
@@ -0,0 +1,21 @@
+package com.vogella.javastarter.loop;
+
+public class Loops {
+    public static void main(String[] args) {
+
+        for (int i=1; i < 10; i++){
+            System.out.println("value of i : " + i);
+        }
+
+        int x = 1;
+
+        while (x < 10) {
+            System.out.println("value of x : " + x);
+            x++;
+        }
+
+
+
+
+    }
+}
